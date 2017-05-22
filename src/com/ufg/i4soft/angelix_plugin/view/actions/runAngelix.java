87022b8ff18f6f4651abb014936ae776cc7bce0c
RenanOfugi <renangyn2010@hotmail.com>
2017-05-22 20:40:18 -0300

    reestruturação de diretorio realizado

---
+++ src/com/ufg/i4soft/angelix_plugin/view/actions/runAngelix.java
@@ -0,0 +1,15 @@
+package com.ufg.i4soft.angelix_plugin.view.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by renan on 22/05/17.
+ */
+public class runAngelix extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
