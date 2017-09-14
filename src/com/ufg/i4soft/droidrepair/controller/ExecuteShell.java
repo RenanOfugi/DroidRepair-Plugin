6ea336866d46b7256719063fb7cc376516375fbf
RenanOfugi <renangyn2010@hotmail.com>
2017-09-14 02:48:30 -0300

    Modificação do algoritmo de execução de comandos em shell script

--- src/com/ufg/i4soft/droidrepair/controller/ExecuteShell.java
+++ src/com/ufg/i4soft/droidrepair/controller/ExecuteShell.java
@@ -11,52 +11,33 @@ class ExecuteShell {
 
     private static final Logger log = Logger.getLogger(ExecuteShell.class.getName());
 
-    String executeCommand(String command) {
-
-        final ArrayList<String> commands = new ArrayList<>();
-        commands.add("/bin/bash");
-        commands.add("-c");
-        commands.add(command);
-
-        BufferedReader reader = null;
-
-        String lastLineText = null;
+    void executeCommand(String command) {
 
         try {
 
-            final ProcessBuilder p = new ProcessBuilder(commands);
-            final Process process = p.start();
-            final InputStream is = process.getInputStream();
-            final InputStreamReader isr = new InputStreamReader(is);
-            reader = new BufferedReader(isr);
+            Process p = Runtime.getRuntime().exec(new String[]{"/bin/bash","-c",command});
 
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
             String line;
 
-            while ((line = reader.readLine()) != null){
+            while ((line = in.readLine()) != null) {
                 System.out.println(line);
-                lastLineText = line;
             }
+
+            BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            while ((line = stderr.readLine()) != null){
+                System.out.println(line);
+            }
+            in.close();
 
         } catch (IOException e) {
 
-            Messages.showMessageDialog( "Erro ao tentar executar o comando no Angelix", "Comando Inválido", Messages.getErrorIcon());
+            Messages.showMessageDialog("Erro ao tentar executar o comando no Angelix", "Comando Inválido", Messages.getErrorIcon());
             throw new ErroExecucaoShellException("Erro ao tentar executar o comando no Angelix");
 
-        }finally {
-            secureClose(reader);
         }
 
-        return lastLineText;
     }
 
-    private void secureClose(final Closeable resource) {
-        try {
-            if (resource != null) {
-                resource.close();
-            }
-        } catch (IOException ex) {
-            log.severe("Erro = " + ex.getMessage());
-        }
-    }
 }
